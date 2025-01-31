da644c36432fa5ed35580f7e4e396d3a9b5c4a09
Anders Eskildsen <22001464+sparkletop@users.noreply.github.com>
2025-01-31 16:21:52 +0100

    housekeeping

--- clean-tex-aux.sh
+++ clean-tex-aux.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd tex
-files="*.aux *.log *.fdb_latexmk *.fls *.out *.pyg *.synctex.gz *.pdf *.toc *.lof *.lol *.xdv"
+files="*.aux *.log *.fdb_latexmk *.fls *.out *.pyg *.synctex.gz *.pdf *.toc *.lof *.lol *.xdv *.bbl *.bcf *.blg *.run.xml"
 rm -f $files
 rm -rf _minted
