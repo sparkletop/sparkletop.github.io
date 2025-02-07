268645c02d6702f70b05472d6d9559bbb384e410
Anders Eskildsen <22001464+sparkletop@users.noreply.github.com>
2025-02-07 15:26:48 +0100

    Adapt cleanup script to new tex file structure

--- clean-tex-aux.sh
+++ clean-tex-aux.sh
@@ -3,4 +3,5 @@
 cd tex
 files="*.aux *.log *.fdb_latexmk *.fls *.out *.pyg *.synctex.gz *.pdf *.toc *.lof *.lol *.xdv *.bbl *.bcf *.blg *.run.xml"
 rm -f $files
-rm -rf _minted
+rm -rf _minted/*
+rm -rf chapters/*.aux
