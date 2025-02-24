47c50140cee2b1b67ab0a331698949ee3d75f7cc
Anders Eskildsen <22001464+sparkletop@users.noreply.github.com>
2025-02-24 14:20:05 +0100

    more aux files

--- clean-tex-aux.sh
+++ clean-tex-aux.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 cd tex
-files="*.aux *.log *.fdb_latexmk *.fls *.out *.pyg *.synctex.gz *.pdf *.toc *.lof *.lol *.xdv *.bbl *.bcf *.blg *.run.xml"
+files="*.aux *.log *.fdb_latexmk *.fls *.out *.pyg *.synctex.gz *.pdf *.toc *.lof *.lol *.xdv *.bbl *.bcf *.blg *.run.xml *-SAVE-ERROR"
 rm -f $files
 rm -rf _minted/*
 rm -rf chapters/*.aux
